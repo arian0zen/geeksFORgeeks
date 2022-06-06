1841a17abc0b2dad88a4427f3a6603387a427dd4
Arian Shaikh <ariansk90@gmail.com>
2022-06-06 21:38:15 +0530

    Create countPairsDivisibleByK_usingHashMap.py

---
+++ countPairsDivisibleByK_usingHashMap.py
@@ -0,0 +1,23 @@
+
+def countKdivPairs(arr, n, k):
+    hash_map = {}
+    ans = 0
+    for each in range(len(arr)):
+        remainder = arr[each] % k
+        if remainder != 0:
+            hash_map[((k-remainder) % k)%k] = ans+1
+        else:
+            ans += hash_map[0]
+        hash_map[remainder] = ++1
+    return ans
+             
+             
+             
+
+arr = [2,2,1,7,5,3]
+n = 6
+k = 4
+countKdivPairs(arr, n, k)
+    
+    
+    #unsolved problem #KeyError: 1
